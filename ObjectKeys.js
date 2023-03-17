64c0def1b924ec27b844a5b5f7659010d1d0b929
hobisognodiunarma <alessandroarmari22@gmail.com>
2023-03-17 14:36:12 +0100

    CHANGES

--- ObjectKeys.js
+++ ObjectKeys.js
@@ -1,53 +1,46 @@
 /*
-Verify if the given string is present in the given
-Object as a Key
+Verify if the a value is present in an Object as a Key
 */
 
-var spike = { name: "spike", surname: "setaro", height: 1.9 };
-var string = "surname";
+var man = { name: "livio", surname: "setaro", height: 1.9 };
+var string = "height";
 
-function isThisStringAmongMyObjectKeys(x) {
-  //Here I convert my given Object Keys in an Array in an Array called newX
-  var newX = Object.keys(x);
+function isThisStringAmongMyObjectKeys(string, object) {
+  //Here I convert my Object Keys in an Array in an Array called newX
+  var newX = Object.keys(object);
 
-  //Here I check if my given string is equal to any of the Array element
+  console.log(newX);
+
+  //Here I check if my value is equal to any of the Array element
   for (i = 0; i < newX.length; i++) {
     if (string == newX[i]) {
-      return (
-        "The Given String is the Key at the position: " +
-        i +
-        " of the given Object"
-      );
+      return "The String is the Key at the position: " + i + " of the Object";
     }
   }
-  return "The value of the given String isn't equal to any of the given Object Keys";
+  return "The value of the String isn't equal to any of the Object Keys";
 }
 
-console.log(isThisStringAmongMyObjectKeys(spike));
+console.log(isThisStringAmongMyObjectKeys(string, man));
 
 /*
-Now we verify if the given string is present in the given
-Object as a Value
+Now we verify if the value is present in the  Object as a Value
 */
 
-var armandeiro = { name: "armando", surname: "picchi", height: 1.7 };
-var string2 = "picchi";
+var man2 = { name: "armando", surname: "picchi", height: 1.7 };
+var string2 = 1.7;
 
-function isThisStringAmongMyObjectValues(y) {
-  //Here I convert my given Object Values in an Array called newY
-  var newY = Object.values(y);
+function isThisStringAmongMyObjectValues(string2, object2) {
+  //Here I convert my  Object Values in an Array called newY
+  var newY = Object.values(object2);
+  console.log(newY);
 
-  //Here I check if my given string is equal to any of the Array element
+  //Here I check if my value is equal to any of the Array element
   for (z = 0; z < newY.length; z++) {
     if (string2 == newY[z]) {
-      return (
-        "The Given String is the Value at the position: " +
-        z +
-        " of the given Object"
-      );
+      return "The value is the Value at the position: " + z + " of the  Object";
     }
   }
-  return "The value of the given String isn't equal to any of the given Object Values";
+  return "The value of the String isn't equal to any of the Object Values";
 }
 
-console.log(isThisStringAmongMyObjectValues(armandeiro));
+console.log(isThisStringAmongMyObjectValues(string2, man2));
